40c69f60b41c45c8c0f94b5b85eb925096135a2e
Kaua Henrique <kauahenriquesx09@gmail.com>
2025-04-03 20:53:44 -0300

    Time

---
+++ Jogador_Time/lista-times-jogadores/Componente/Time.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { FlatList, StyleSheet, Text } from 'react-native'
+import { Card } from 'react-native-paper'
+import Municipio from './Jogador'
+
+
+export default function Time(props) {
+
+
+  const { nome, anoFundacao, mascote, imagem, jogadores } = props
+
+
+  return (
+    <Card style={{ margin: 10 }}>
+      <Card.Title title={nome} />
+      <Card.Content>
+        <Text>Ano de Fundação: {anoFundacao}</Text>
+      </Card.Content>
+      <Card.Content>
+        <Text>Mascote: { mascote}</Text>
+      </Card.Content>
+      <Card.Cover source={{ uri: imagem }} />
+      <Card.Actions>
+        <FlatList
+          horizontal
+          data={jogadores}
+          renderItem={({ item }) => (
+            <Municipio
+              nome={item.nome}
+              imagem={item.imagem}
+            />
+          )}
+        />
+      </Card.Actions>
+    </Card>
+  )
+}
+
+
+const styles = StyleSheet.create({})
+
